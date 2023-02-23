abb01a26e155932281da056689dd94c4c181aad5
zliu102 <zliu102@hawk.iit.edu>
2023-02-23 07:44:33 -0600

    a

--- reservoir_sampling.c
+++ reservoir_sampling.c
@@ -136,10 +136,10 @@ finalize_trans_crimes_c(PG_FUNCTION_ARGS)
                 bytea  *addr = (bytea *) PG_GETARG_BYTEA_P(0);
                 st->reservoir = MyNew_intArrayType(100);
                 state_c **pp2 = &st;    
-                memcpy(pp2,VARDATA(addr),sizeof(*pp2));
+                //memcpy(pp2,VARDATA(addr),sizeof(*pp2));
                 //int len = sizeof(struct state_c);
                 //memcpy(st, (state_c *)((uintptr_t) *(addr->vl_dat)), 100);
-                //sscanf(VARDATA(addr), "%p", (void**)&st);
+                sscanf(VARDATA(addr), "%p", (void**)&st);
                 num = st->reservoir_size;
                 dr = (int64 *) ARR_DATA_PTR(st->reservoir); 
                 
