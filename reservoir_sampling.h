1b65aac65f24617a779c40984816fd0e0fb0611c
zliu102 <zliu102@hawk.iit.edu>
2023-02-25 08:00:59 -0600

    a

--- reservoir_sampling.h
+++ reservoir_sampling.h
@@ -6,6 +6,6 @@
 
 extern Datum add_ab(PG_FUNCTION_ARGS);
 extern Datum res_trans_crimes_c(PG_FUNCTION_ARGS);
-extern ArrayType finalize_trans_crimes_c(PG_FUNCTION_ARGS);
+extern Datum finalize_trans_crimes_c(PG_FUNCTION_ARGS);
 
 
