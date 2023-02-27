3374953083978b6316cb0e30ef8ec3e1191d3c4e
zliu102 <zliu102@hawk.iit.edu>
2023-02-27 07:14:52 -0600

    a

--- reservoir_sampling.c
+++ reservoir_sampling.c
@@ -118,6 +118,7 @@ res_trans_crimes_c(PG_FUNCTION_ARGS)
             }
             s->poscnt ++;
         }
+        elog(INFO, "----------------"); 
         //pfree(s);
         PG_RETURN_BYTEA_P(addr);
 }
